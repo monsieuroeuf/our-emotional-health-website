645b759a1575834a3e64dba8720c6eabb9da4f59
Ian Haigh <mail@ianhaigh.com>
2012-08-17 16:40:21 +1000

    Added some jQuery! Drop shadows! Rounded corners!

---
+++ js/robin.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$(".alert-outer").fadeTo('slow', 0.99);
+	divHeight = $('.alert-outer').height();
+	headingHeight = $('.alert-outer h3').height();
+	paddingAmt = (divHeight - headingHeight) / 2;
+	$('.alert-outer h3').css({
+	  "margin-top": paddingAmt
+	})
+});
